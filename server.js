f1d6cca8c08e90f5b637eb2d50bb9433713e0126
Darren James <darrenjames.dalino@gmail.com>
2020-09-01 02:15:27 +0800

    version 2.0.2

--- server.js
+++ server.js
@@ -8,25 +8,9 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`);
-});
-
 //routes
 const upload = require("./routes/upload.routes");
 app.use("/api/upload", upload);
-
-const mongoURI =
-  "mongodb+srv://djdalino:TpW5Z6aYjCENviOt@cluster3-hngry.mongodb.net/test?retryWrites=true&w=majority";
-mongoose.connect(
-  mongoURI || process.env.MONGODB_URL,
-  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
-  err => {
-    if (err) return console.error(err);
-    console.log("connected to the database!");
-  }
-);
 
 const root = require("path").join(__dirname, "/client/build");
 
@@ -38,3 +22,19 @@ app.get("*", (req, res) => {
 // app.get("*", (req, res) => {
 //   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 // });
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
+
+const mongoURI =
+  "mongodb+srv://djdalino:TpW5Z6aYjCENviOt@cluster3-hngry.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(
+  mongoURI || process.env.MONGODB_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+  err => {
+    if (err) return console.error(err);
+    console.log("connected to the database!");
+  }
+);
